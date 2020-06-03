9bd646a80562b19e236831702b8feb88c6fca219
shivam7374 <shivamsingla491@gmail.com>
2020-06-03 13:12:56 +0530

    Cart Backend Connected

--- User-Auth/public/js/cart.js
+++ User-Auth/public/js/cart.js
@@ -22,10 +22,37 @@ set = new Set(res3)
     // {
     //     console.log(iterator.next().value)
     // }
-    for (item of set.values())
+    for (id of set.values())
 {     //document.write(item+",");
     //  document.getElementById('demo').innerHTML="The set value is: "+ item;
-    console.log(item)
+    console.log(id)
+    $.get('/api/myposts/'+id, (products) => {
+        console.log("++++++++++++++++++")
+        console.log(products)
+        console.log("++++++++++++++++++")
+        for (let p of products) {
+          $('#products-container').append(
+            $(`
+            <div class="col-4">
+              <div class="card m-2"> 
+              <a href="/${p.id}/product">
+              <img src="${p.avatar}" class="card-img-top">
+              </a>
+                <div class="card-body">
+                  <h5 class="card-title">${p.name}</h5>
+                  <p class="card-text">
+                    ${p.company}
+                  </p>
+                  <h6 class="card-subtitle mb-2 text-muted">Rs. ${p.price}</h6>
+                  <h6 class="card-subtitle mb-2 text-muted">${p.username}</h6>
+                  </div>
+              </div>
+            </div>
+            
+            `)
+          )
+        }
+      })
 } 
 
 })
