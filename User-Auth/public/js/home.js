3c0fa56b17a5964083ba840a95d9340672700e04
shivam7374 <shivamsingla491@gmail.com>
2020-06-05 11:58:55 +0530

    Cart button corrected and searchbar added

--- User-Auth/public/js/home.js
+++ User-Auth/public/js/home.js
@@ -1,6 +1,6 @@
   let linkcart="/0/cart"
   $(()=>{
-    $('#cartlist').hide()
+    // $('#cartlist').hide()
   
     let cart=document.getElementById('cart')
     setInterval(function(){cart.innerHTML=`<a class="navbar-brand" href=${linkcart}>
@@ -29,7 +29,7 @@
                 </p>
                 <h6 class="card-subtitle mb-2 text-muted">Rs. ${p.price}</h6>
                 <h6 class="card-subtitle mb-2 text-muted">${p.username}</h6>
-                <button onclick="btnclicked(${p.id})">Addtocart</button>
+                <button onclick="btnclicked(${p.id})" id="${p.id}">Add to Cart</button>
                 </div>
             </div>
           </div>
@@ -41,8 +41,47 @@
   }
   // let pos=2
 function btnclicked(product){
-  $('#cartlist').show()
-  $('#cartvalue').append(`
-  <li>${product}</li>`)
+
   linkcart=linkcart.substr(0,2) +"-"+`${product}`+ linkcart.substr(2)
+  // $('#cartlist').show()
+  document.getElementById(`${product}`).innerText="Added to Cart"
+  // $('#cartvalue').append(`
+  // <li>${product}</li>`)
+}
+var TRange=null;
+
+function findString (str) {
+ if (parseInt(navigator.appVersion)<4) return;
+ var strFound;
+ if (window.find) {
+
+  // CODE FOR BROWSERS THAT SUPPORT window.find
+
+  strFound=self.find(str);
+  if (!strFound) {
+   strFound=self.find(str,0,1);
+   while (self.find(str,0,1)) continue;
+  }
+ }
+ else if (navigator.appName.indexOf("Microsoft")!=-1) {
+
+  // EXPLORER-SPECIFIC CODE
+
+  if (TRange!=null) {
+   TRange.collapse(false);
+   strFound=TRange.findText(str);
+   if (strFound) TRange.select();
+  }
+  if (TRange==null || strFound==0) {
+   TRange=self.document.body.createTextRange();
+   strFound=TRange.findText(str);
+   if (strFound) TRange.select();
+  }
+ }
+ else if (navigator.appName=="Opera") {
+  alert ("Opera browsers not supported, sorry...")
+  return;
+ }
+ if (!strFound) alert ("String '"+str+"' not found!")
+ return;
 }
