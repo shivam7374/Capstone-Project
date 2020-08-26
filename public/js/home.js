1c5418b693bf4eddcedee5842a3d30f2e5b9c019
shivam7374 <shivamsingla491@gmail.com>
2020-08-26 11:06:07 +0530

    Cart Icon

--- public/js/home.js
+++ public/js/home.js
@@ -1,25 +1,26 @@
-  let linkcart="/0/cart"
+let linkcart = "/0/cart";
 
-  var set1 = new Set();
-  $(()=>{
-    // $('#cartlist').hide()
-  
-    let cart=document.getElementById('cart')
-    setInterval(function(){cart.innerHTML=`<a class="navbar-brand" href=${linkcart}>
+var set1 = new Set();
+$(() => {
+  // $('#cartlist').hide()
+
+  let cart = document.getElementById("cart");
+  setInterval(function () {
+    cart.innerHTML = `<a class="navbar-brand" href=${linkcart}>
     &nbsp;
-    <img class="d-inline cart" src="../assets/cart.png">Cart</a>`}, 1000)
-  })
-  
-  
-  function loadProducts() {
-    //   console.log("JS Loaded Successfully")
-    $.get('/product', (products) => {
-      console.log("++++++++++++++++++")
-      console.log(products)
-      console.log("++++++++++++++++++")
-      for (let p of products) {
-        $('#products-container').append(
-          $(`
+    <img class="d-inline cart" src="https://t4.ftcdn.net/jpg/01/63/42/79/240_F_163427943_W56xtj7YydS4YujdUqQot94IINtt91FV.jpg">Cart</a>`;
+  }, 1000);
+});
+
+function loadProducts() {
+  //   console.log("JS Loaded Successfully")
+  $.get("/product", (products) => {
+    console.log("++++++++++++++++++");
+    console.log(products);
+    console.log("++++++++++++++++++");
+    for (let p of products) {
+      $("#products-container").append(
+        $(`
           <div class="col-md-4 d-flex p-2">
             <div class="card mb-2"id="prod"> 
             <a href="/${p.id}/product">
@@ -38,85 +39,81 @@
           </div>
           
           `)
-        )
-      }
-      set1.add(0)
-      $('#carousel-inner').append(
-        $(`
+      );
+    }
+    set1.add(0);
+    $("#carousel-inner").append(
+      $(`
         <div class="carousel-item active">
         <a href="/${products[0].id}/product">
         <img class="d-block w-75 crousel" src="${products[0].avatar}" alt="Second slide">
         </a>
         </div>
-      `))
-      for(let i=0;i<5;i++)
-      {
-        set1.add(parseInt(Math.random()*10)%5)
-        // console.log(products[x].avatar);
-        // $('#carousel-inner').append(
-        //   $(`
-        //   <div class="carousel-item">
-        //   <img class="d-block w-100 crousel" src="${products[parseInt(Math.random()*10)%3].avatar}" alt="Second slide">
-        //   </div>
-        // `))
-      }
-      for (id of set1.values())
-      {
-        if(id!=0){
-        $('#carousel-inner').append(
+      `)
+    );
+    for (let i = 0; i < 5; i++) {
+      set1.add(parseInt(Math.random() * 10) % 5);
+      // console.log(products[x].avatar);
+      // $('#carousel-inner').append(
+      //   $(`
+      //   <div class="carousel-item">
+      //   <img class="d-block w-100 crousel" src="${products[parseInt(Math.random()*10)%3].avatar}" alt="Second slide">
+      //   </div>
+      // `))
+    }
+    for (id of set1.values()) {
+      if (id != 0) {
+        $("#carousel-inner").append(
           $(`
           <div class="carousel-item">
           <a href="/${products[id].id}/product">
           <img class="d-block w-75 crousel" src="${products[id].avatar}" alt="Second slide">
           </a>
           </div>
-        `))
-      }   }
-    })
-  }
-  // let pos=2
-function btnclicked(product){
-
-  linkcart=linkcart.substr(0,2) +"-"+`${product}`+ linkcart.substr(2)
+        `)
+        );
+      }
+    }
+  });
+}
+// let pos=2
+function btnclicked(product) {
+  linkcart = linkcart.substr(0, 2) + "-" + `${product}` + linkcart.substr(2);
   // $('#cartlist').show()
-  document.getElementById(`${product}`).innerText="Added to Cart"
+  document.getElementById(`${product}`).innerText = "Added to Cart";
   // $('#cartvalue').append(`
   // <li>${product}</li>`)
 }
-var TRange=null;
+var TRange = null;
 
-function findString (str) {
- if (parseInt(navigator.appVersion)<4) return;
- var strFound;
- if (window.find) {
+function findString(str) {
+  if (parseInt(navigator.appVersion) < 4) return;
+  var strFound;
+  if (window.find) {
+    // CODE FOR BROWSERS THAT SUPPORT window.find
 
-  // CODE FOR BROWSERS THAT SUPPORT window.find
+    strFound = self.find(str);
+    if (!strFound) {
+      strFound = self.find(str, 0, 1);
+      while (self.find(str, 0, 1)) continue;
+    }
+  } else if (navigator.appName.indexOf("Microsoft") != -1) {
+    // EXPLORER-SPECIFIC CODE
 
-  strFound=self.find(str);
-  if (!strFound) {
-   strFound=self.find(str,0,1);
-   while (self.find(str,0,1)) continue;
+    if (TRange != null) {
+      TRange.collapse(false);
+      strFound = TRange.findText(str);
+      if (strFound) TRange.select();
+    }
+    if (TRange == null || strFound == 0) {
+      TRange = self.document.body.createTextRange();
+      strFound = TRange.findText(str);
+      if (strFound) TRange.select();
+    }
+  } else if (navigator.appName == "Opera") {
+    alert("Opera browsers not supported, sorry...");
+    return;
   }
- }
- else if (navigator.appName.indexOf("Microsoft")!=-1) {
-
-  // EXPLORER-SPECIFIC CODE
-
-  if (TRange!=null) {
-   TRange.collapse(false);
-   strFound=TRange.findText(str);
-   if (strFound) TRange.select();
-  }
-  if (TRange==null || strFound==0) {
-   TRange=self.document.body.createTextRange();
-   strFound=TRange.findText(str);
-   if (strFound) TRange.select();
-  }
- }
- else if (navigator.appName=="Opera") {
-  alert ("Opera browsers not supported, sorry...")
+  if (!strFound) alert("String '" + str + "' not found!");
   return;
- }
- if (!strFound) alert ("String '"+str+"' not found!")
- return;
 }
